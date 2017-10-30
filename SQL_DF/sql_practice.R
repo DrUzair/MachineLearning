b1a2178effdbe914fd90be3bcd67fbe2df375091
Dr Uzair Ahmad <DrUzair@users.noreply.github.com>
2017-10-30 17:08:25 -0400

    Add files via upload

---
+++ SQL_DF/sql_practice.R
@@ -0,0 +1,22 @@
+# install.packages('sqldf')
+# library('sqldf)
+
+drivers <- read.csv('c:\\sql_practice\\drivers.csv')
+timesheet <- read.csv('c:\\sql_practice\\timesheet.csv')
+
+# selecting all columns
+sqldf('select * from drivers')
+sqldf('select * from timesheet')
+
+# selecting named columns
+sqldf('select driverId, name from drivers')
+
+# Using arithmetic operators (sum, mean, std)
+# SUM                --> SUM(column_name)
+sqldf('select sum(mileslogged) AS MILES, sum(hourslogged) AS hours from timesheet')
+# AVERAGE (MEAN)     --> avg(column_name)
+# STANDARD DEVIATION --> stdev(column_name)
+sqldf('select sum(mileslogged) AS TOTAL_HOURS, avg(hourslogged) AS AVG_HOURS, stdev(hourslogged) AS ST_DEV_HOURS from timesheet')
+
+# GROUP BY Operator
+sqldf('select driverId AS DRIVER, sum(hourslogged) AS TOTAL_HOURS from timesheet GROUP BY driverId')
