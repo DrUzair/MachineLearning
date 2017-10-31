13e410125fe472737792e021ec7154558f3f22c6
Dr Uzair Ahmad <DrUzair@users.noreply.github.com>
2017-10-31 13:17:43 -0400

    Add files via upload

---
+++ Perf_Eval/confusion_matrix.r
@@ -0,0 +1,14 @@
+library('e1071')
+rn_train <- sample(nrow(iris), floor(nrow(iris)*.70))
+train <- iris[rn_train,]
+test <- iris[-rn_train,]
+model <- naiveBayes(Species~., data=train)
+predictions <- predict(model, test)
+table(True = test$Species, Pred = predictions)
+
+##############OUTPUT#########################
+                          Pred
+#  True         setosa versicolor virginica
+#  setosa         19          0         0
+#  versicolor      0         10         1
+#  virginica       0          1        14
