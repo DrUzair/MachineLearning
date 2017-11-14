bfd34c13ecf3f19ea2f5e53933e9de087901604e
Dr Uzair Ahmad <DrUzair@users.noreply.github.com>
2017-11-15 02:24:12 +0800

    Update autocorrfun.R

--- TimeSeries/autocorrfun.R
+++ TimeSeries/autocorrfun.R
@@ -8,23 +8,18 @@
  
 # following code is a simple example how acf can be calculated (without using acf function)
 
-x <- as.vector(1:10)
-
-as.vector(lag(x,3))
-
-acf(x, plot=F)
-y <- 1:10
-
+# create a time series y containing 10 values
 y <-ts(data = 1:10, start = 1)
-
+# calculate var and mean of the time series y
 ts_variance <- var(y)
 ts_mean <- mean(y)
 N <- length(y)
-
+# create a list ac_vals to save autocorrelation values for each lag
 ac_vals <- rep(NA, N)
+# following loop with calcular autocorrelation for each lag 0 to N (0 means no lag, so autocorrelation should be 1)
 for(lag in 0:N-1){
   Y <- ts.union(y, lag(y, k =  lag))
-  ac_vals[lag+1] <- (sum((Y[, 1] - m) * (Y[, 2] - m), na.rm = T)/(N-1)) / ts_variance
+  ac_vals[lag+1] <- (sum((Y[, 1] - ts_mean) * (Y[, 2] - ts_mean), na.rm = T)/(N-1)) / ts_variance
 }
 ac_vals
 # the output is same as acf
